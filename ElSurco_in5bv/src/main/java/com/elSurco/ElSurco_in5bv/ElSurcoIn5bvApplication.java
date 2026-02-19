db8e4b109ed8d4cbd5df397dde97d027606ab936
Pablo <prequena-2025262@kinal.edu.gt>
2026-02-19 13:30:15 -0600

    Base del proyecto

---
+++ ElSurco_in5bv/src/main/java/com/elSurco/ElSurco_in5bv/ElSurcoIn5bvApplication.java
@@ -0,0 +1,13 @@
+package com.elSurco.ElSurco_in5bv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElSurcoIn5bvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElSurcoIn5bvApplication.class, args);
+	}
+
+}
---
+++ ElSurco_in5bv/src/test/java/com/elSurco/ElSurco_in5bv/ElSurcoIn5bvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elSurco.ElSurco_in5bv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElSurcoIn5bvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
