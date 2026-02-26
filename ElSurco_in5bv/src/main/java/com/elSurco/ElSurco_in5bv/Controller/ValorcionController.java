f145025cd3ee45f9f220da1024528e68d523f2cb
Randolf Bran <randolfbranreyes@gmail.com>
2026-02-26 16:00:42 -0600

    Realize las validaciones

--- ElSurco_in5bv/src/main/java/com/elSurco/ElSurco_in5bv/Controller/ValorcionController.java
+++ ElSurco_in5bv/src/main/java/com/elSurco/ElSurco_in5bv/Controller/ValorcionController.java
@@ -1,4 +1,43 @@
 package com.elSurco.ElSurco_in5bv.Controller;
+import com.elSurco.ElSurco_in5bv.Entity.Valoracion;
+import com.elSurco.ElSurco_in5bv.Service.ValoracionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+@RestController
+@RequestMapping("/api/voloracion")
 public class ValorcionController {
+    private final ValoracionService valoracionService;
+
+    public ValorcionController(ValoracionService valoracionService){
+        this.valoracionService = valoracionService;
+    }
+
+    @GetMapping("/get")
+    public List<Valoracion> listar(){
+        return valoracionService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Valoracion obtener(@PathVariable Integer id){
+        return valoracionService.obtenerPorId(id);
+    }
+
+    @PostMapping("/post")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Valoracion crear(@RequestBody Valoracion valoracion){
+        return  valoracionService.agregar(valoracion);
+    }
+
+    @PutMapping("/put/{id}")
+    public Valoracion actualizar(@PathVariable("Id") Integer id, @RequestBody Valoracion valoracion){
+        return valoracionService.actualizar(id, valoracion);
+    }
+
+    @DeleteMapping("/{id}")
+    public Valoracion eliminar(@PathVariable Integer id){
+        valoracionService.eliminar(id);
+        return null;
+    }
 }
